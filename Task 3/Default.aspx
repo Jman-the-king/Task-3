89738d95e38676ac3c0ed409fe76ffa6ded0c289
Jman-the-king <jcleafe@gmail.com>
2025-01-25 18:00:37 +0200

    Add project files.

---
+++ Task 3/Default.aspx
@@ -0,0 +1,95 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Task_3._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<form id="form1" enctype="multipart/form-data">
+
+<div>
+
+<label for="Id">User ID(For adding, updating and removing):</label> <br />
+
+<asp:TextBox ID="IdTextBox" runat="server"></asp:TextBox>
+
+<br />
+
+
+<label for="Firstname">First Name:</label><br/>
+
+<asp:TextBox ID="FirstNameTextBox" runat="server"></asp:TextBox>
+
+<br />
+
+
+<label for="Lastname">Last Name:</label><br/>
+
+<asp:TextBox ID="LastNameTextBox" runat="server"></asp:TextBox>
+
+<br />
+
+
+<label for="Numberofgroups">Number of group(s):</label><br/>
+
+<asp:TextBox ID="NumberOfGroupsTextBox" runat="server"></asp:TextBox>
+
+<br />
+
+
+<label for="Groupname">Group Name(s):</label><br/>
+
+<asp:TextBox ID="GroupNameTextBox" runat="server"></asp:TextBox>
+
+<br />
+
+
+
+
+
+<asp:Button ID="SubmitButton" runat="server" Text="Add" OnClick="SubmitButton_Click" />
+    
+<asp:Button ID="RemoveButton" runat="server" Text="Remove" OnClick="RemoveButton_Click" />
+
+<asp:Button ID="UpdateButton" runat="server" Text="Update" OnClick="UpdateButton_Click" />
+
+<br /><br />
+
+
+<asp:GridView ID="UserGridView" runat="server" AutoGenerateColumns="False" OnSelectedIndexChanged="UserGridView_SelectedIndexChanged">
+
+<Columns>
+
+        <asp:BoundField DataField="UserId" HeaderText="User ID" />
+
+        <asp:BoundField DataField="FirstName" HeaderText="First Name" />
+
+        <asp:BoundField DataField="LastName" HeaderText="Last Name" />
+    
+        <asp:BoundField DataField="GroupName" HeaderText="Group Names" />
+
+        <asp:BoundField DataField="NumberOfGroups" HeaderText="Number Of Groups" />
+
+        <asp:CommandField ShowSelectButton="True" />
+
+</Columns>
+
+</asp:GridView>
+
+<br /><br />
+
+
+<asp:Label ID="UserCountLabel" runat="server" Text="Total Users: 0" Style="font-weight:bold;"></asp:Label>
+
+
+</div>
+
+</form>
+
+</body>
+
+</html>
+</asp:Content>
+
---
+++ Task 3/Default.aspx.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services.Description;
+
+namespace Task_3
+{
+    public partial class _Default : Page
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["UsersDBConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindGrid();
+            UpdateUserCount();
+        }
+
+        ///-----------------------------------------------------------------------------------------------------------////
+
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            string firstName = FirstNameTextBox.Text;
+            string lastName = LastNameTextBox.Text;
+            string groupName = GroupNameTextBox.Text;
+            int numberOfGroups;
+            dynamic userId = IdTextBox.Text;
+
+
+
+            if (int.TryParse(NumberOfGroupsTextBox.Text, out numberOfGroups))
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO Users (UserId, FirstName, LastName, GroupName, NumberOfGroups) VALUES (@UserId, @FirstName, @LastName, @GroupName, @NumberOfGroups)";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    command.Parameters.AddWithValue("@UserId", userId);
+                    command.Parameters.AddWithValue("@FirstName", firstName);
+                    command.Parameters.AddWithValue("@LastName", lastName);
+
+                    string groupNamesParam = string.IsNullOrEmpty(groupName) ? "" : groupName;
+                    command.Parameters.AddWithValue("@GroupName", groupNamesParam);
+
+                    command.Parameters.AddWithValue("@NumberOfGroups", numberOfGroups);
+
+                                    
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                    ClearFields();
+                    BindGrid();
+                }
+            }
+            else
+            {
+                if(!int.TryParse(NumberOfGroupsTextBox.Text, out _))
+                {
+                    NumberOfGroupsTextBox.Text = "Invalid input. Please check all fields.";
+                }
+                
+            }
+
+        }
+
+        ///-----------------------------------------------------------------------------------------------------------////
+
+        protected void RemoveButton_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(IdTextBox.Text);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Users WHERE UserId = @UserId";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@UserId", id);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            ClearFields();
+            BindGrid();
+        }
+
+        protected void UpdateButton_Click(object sender, EventArgs e)
+        {
+            string firstName = FirstNameTextBox.Text;
+            string lastName = LastNameTextBox.Text;
+            string groupName = GroupNameTextBox.Text;
+            int numberOfGroups;
+            dynamic userId = IdTextBox.Text;
+
+            if (int.TryParse(NumberOfGroupsTextBox.Text, out numberOfGroups)) 
+            {
+
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "UPDATE Users SET firstName = @FirstName, lastName = @LastName, groupName = @GroupName, numberOfGroups = @NumberOfGroups WHERE UserId = @UserId";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@UserId", userId);
+                    command.Parameters.AddWithValue("@FirstName", firstName);
+                    command.Parameters.AddWithValue("@LastName", lastName);
+
+                    string groupNamesParam = string.IsNullOrEmpty(groupName) ? "" : groupName;
+                    command.Parameters.AddWithValue("@GroupName", groupNamesParam);
+
+                    command.Parameters.AddWithValue("@NumberOfGroups", numberOfGroups);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+
+                ClearFields();
+                BindGrid();
+            }
+        }
+
+
+        private void BindGrid()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Users";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                UserGridView.DataSource = dt;
+                UserGridView.DataBind();
+            }
+
+            UpdateUserCount();
+        }
+
+        ///-----------------------------------------------------------------------------------------------------------////
+        
+        private void ClearFields()
+        {
+            IdTextBox.Text = string.Empty;
+            FirstNameTextBox.Text = string.Empty;
+            LastNameTextBox.Text = string.Empty;
+            NumberOfGroupsTextBox.Text = string.Empty;
+            GroupNameTextBox.Text = string.Empty;
+        }
+
+        ///-----------------------------------------------------------------------------------------------------------////
+
+        
+        protected void UserGridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            GridViewRow row = UserGridView.SelectedRow;
+            IdTextBox.Text = row.Cells[0].Text;
+            FirstNameTextBox.Text= row.Cells[1].Text;
+            LastNameTextBox.Text = row.Cells[2].Text;
+            GroupNameTextBox.Text = row.Cells[3].Text;
+            NumberOfGroupsTextBox.Text = row.Cells[4].Text;            
+        }
+
+        ///-----------------------------------------------------------------------------------------------------------////
+
+
+        private void UpdateUserCount()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT COUNT(*) FROM Users";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                int userCount = (int)command.ExecuteScalar(); 
+                UserCountLabel.Text = $"Total Users: {userCount}"; 
+            }
+        }
+
+    }
+}
